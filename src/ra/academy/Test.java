ae7a9f316e687409e736d5ff96565e6b5707a59d
hunghx <120539776+hunghx@users.noreply.github.com>
2022-12-18 13:58:18 +0700

    Add files via upload

---
+++ src/ra/academy/Test.java
@@ -0,0 +1,17 @@
+package ra.academy;
+
+public class Test {
+    public static void main(String[] args) {
+        // Write your code here
+        int i = 10; // Khai báo biến i kiểu int có giá trị khởi tạo là 10.
+        float f = 20.5f; // Khai báo biến f kiểu float có giá trị khởi tạo là 20,5 .
+        boolean b = true; // Khai báo biến b kiêu boolean có giá trị khởi tạo là true .
+        char c = 'c'; // Khai báo biến c kiểu char có giá trị khởi tạo là 'c' .
+        String s = "Hà Nội"; // Khai báo biến s kiểu String có giá trị khởi tạo là "Hà Nội" .
+        System.out.println("i = "+i);
+        System.out.println("f = "+f);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        System.out.println("s = "+s);
+    }
+}
